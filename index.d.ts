f5c7ec5cb9d36f52e13614b57b355807c8d72977
LarryZhu-dev <larryzhu@foxmail.com>
2024-04-15 15:00:59 +0800

    fix: 修复ts错误、新增git仓库地址

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ declare interface autolog {
    * - type（可选）：输出类型 默认是 "log"
    * - time（可选）：停留时间 默认是 2500
    */
-  log(text: string, type?: "log" | "warn" | "warning" | "error" | "info" | "success" | "ok" | "done", time: number): void;
+  log(text: string, type?: "log" | "warn" | "warning" | "error" | "info" | "success" | "ok" | "done", time?: number): void;
 }
 declare const autolog: autolog;
 export default autolog;
