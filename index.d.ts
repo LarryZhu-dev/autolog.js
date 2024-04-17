e50474e933c637f20edbc2c6d436d8070ec33c83
LarryZhu-dev <larryzhu@foxmail.com>
2024-04-17 13:33:02 +0800

    fix: 修复ts提示

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
+type logType = "log" | "warn" | "warning" | "error" | "info" | "success" | "ok" | "done";
+type logTime = number;
 declare interface autolog {
   /**
    * - text（必填）：需要输出的文本
    * - type（可选）：输出类型 默认是 "log"
    * - time（可选）：停留时间 默认是 2500
    */
-  log(text: string, type?: "log" | "warn" | "warning" | "error" | "info" | "success" | "ok" | "done", time?: number): void;
+  log(text: string, type?: logType | logTime, time?: number): void;
 }
 declare const autolog: autolog;
 export default autolog;
