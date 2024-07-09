4fcdc220c4d4958a03b72d6d1f3e5dd3253750f6
Larry Zhu <larryzhu@foxmail.com>
2024-07-09 11:37:49 +0800

    fix: type error & fix customSvg's backgroundColor

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,35 +1,35 @@
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const { resolveRoot } = require('./util');
-const webpackCommonConfig = require('./webpack.common.conf');
+const { resolveRoot } = require("./util");
+const webpackCommonConfig = require("./webpack.common.conf");
 
-module.exports = merge(webpackCommonConfig,{
-  mode: 'development',
-  entry: resolveRoot('./dev/index.ts'),
+module.exports = merge(webpackCommonConfig, {
+  mode: "development",
+  entry: resolveRoot("./dev/index.ts"),
   output: {
-    filename: 'autolog.min.js',
-    path: resolveRoot(''),
-    library: 'autolog',
-    libraryTarget: 'umd',
+    filename: "autolog.min.js",
+    path: resolveRoot(""),
+    library: "autolog",
+    libraryTarget: "umd",
     environment: {
       const: false,
-      arrowFunction: false
-    }
+      arrowFunction: false,
+    },
   },
   devServer: {
     compress: true,
     port: 9000,
     static: {
       watch: true,
-      directory: resolveRoot('./dev'),
+      directory: resolveRoot("./dev"),
     },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'autolog.js dev',
-      favicon: resolveRoot('./autolog.svg'),
-      template: resolveRoot('./dev/index.html'),
+      title: "autolog.js dev",
+      favicon: resolveRoot("./autolog.svg"),
+      template: resolveRoot("./dev/index.html"),
     }),
   ],
 });
